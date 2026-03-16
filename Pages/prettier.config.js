eaae19b8decc23f01ab92ede372e057c3f464396
Jamie <fleming1411@yahoo.co.uk>
2026-03-16 07:35:38 +0000

    Started rewriting HTML for mobile first with tailwindcss"

---
+++ Pages/prettier.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  tabWidth: 2,      // 2-space indents
+  semi: true,       // Add semicolons
+  singleQuote: true // Single quotes
+};
