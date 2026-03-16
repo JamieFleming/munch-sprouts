135b8ac662a77778eef809c46c9032e1d7eb6b85
Jamie <fleming1411@yahoo.co.uk>
2026-03-16 20:26:21 +0000

    File structure tidying, tailwind prettier configured

--- Pages/prettier.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  plugins: ['prettier-plugin-tailwindcss'],
-  tabWidth: 2,      // 2-space indents
-  semi: true,       // Add semicolons
-  singleQuote: true // Single quotes
-};
